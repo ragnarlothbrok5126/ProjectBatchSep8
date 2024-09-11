803821652010ef2d72bfa50b1ba5cd03de09b14f
ragnarlothbrok5126 <rakeshrocky09876@gmail.com>
2024-09-11 13:57:48 +0530

    Rocky code

--- src/test/java/org/test/Sampletest.java
+++ src/test/java/org/test/Sampletest.java
@@ -2,4 +2,18 @@ package org.test;
 
 public class Sampletest {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("Ragul");
+		System.out.println("Ranjith");
+		System.out.println("Barath");
+		
+		
+		
+		
+	}
+	
+	
+	
 }
